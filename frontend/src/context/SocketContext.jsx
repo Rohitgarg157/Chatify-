26c9f258f64f649efe1adeaefab85894c61b0814
Rohit Garg <Rohitgarg157@gmail.com>
2024-02-23 19:58:23 +0530

    Socket url updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -14,7 +14,7 @@ export const SocketContextProvider=({children})=>{
     const {authUser}=useAuthContext();
     useEffect(()=>{
         if(authUser){
-            const socket=io("http://localhost:5000",
+            const socket=io("https://chatify-t1l6.onrender.com",
             {
                 query:{
                     userId:authUser._id  ,         
